a6e07ce0451b8dc5e844821ff4e9cc8b3112bb33
Kimi Syrjä <kimi.syrja@gmail.com>
2021-11-05 15:29:22 +0200

    Windows-update
    + Windows details check
    + CPU detail check

--- Datacase/Datacase/Program.cs
+++ Datacase/Datacase/Program.cs
@@ -4,9 +4,56 @@ namespace Datacase
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // CPU check
+            string CPU = System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            Console.WriteLine(CPU);
+
+            // 32bit/64bit
+
+
+
+            Console.WriteLine("-- Operating System Detaiils --");
+
+            OperatingSystem os = Environment.OSVersion;
+
+            Console.WriteLine("OS Version: " + os.Version.ToString());
+
+            Console.WriteLine("OS Platoform: " + os.Platform.ToString());
+
+            Console.WriteLine("OS SP: " + os.ServicePack.ToString());
+
+            Console.WriteLine("OS Version String: " + os.VersionString.ToString());
+
+            Console.WriteLine();
+
+            Version ver = os.Version;
+
+            Console.WriteLine("Major version: " + ver.Major);
+
+            Console.WriteLine("Major Revision: " + ver.MajorRevision);
+
+            Console.WriteLine("Minor version: " + ver.Minor);
+
+            Console.WriteLine("Minor Revision: " + ver.MinorRevision);
+
+            Console.WriteLine("Build: " + ver.Build);
+
+            Console.WriteLine("----");
+
+            bool is64 = System.Environment.Is64BitOperatingSystem;
+            if (is64 == true) {
+                Console.WriteLine("64bit");
+            } else
+            {
+                Console.WriteLine("32bit");
+            }
+            
         }
+
+
     }
 }
