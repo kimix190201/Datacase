e8e161157fc7c55634f96fde45c665c350ce7081
Arseniy Sergeenko <arseniy20100@student.hamk.fi>
2021-12-17 17:41:04 +0200

    Program now only displays CPU temperature

--- CPUTemp2/CPUTemp2/Program.cs
+++ CPUTemp2/CPUTemp2/Program.cs
@@ -12,36 +12,53 @@ namespace CPUTemp2
     {
         static float cpuTemp;
 
-
-        // Instantiate the computer object to expose hardware (CPU) information
+        // Instantiate the computer object to expose hardware information
         static Computer c = new Computer()
         {
             CPUEnabled = true
         };
 
+
         
+        public class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer c)
+            {
+                c.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+
+        }
+
         static void ReportSystemInfo()
         {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
             Database databaseObject = new Database();
             string query = "INSERT INTO CPU ('temperature') VALUES (@temperature)";
             SQLiteCommand command = new SQLiteCommand(query, databaseObject.connection);
             databaseObject.OpenConnection();
+            c.Accept(updateVisitor);
 
-            foreach (var hardware in c.Hardware)
+            for (float i = 0; i < c.Hardware.Length; i++)
             {
-                if (hardware.HardwareType == HardwareType.CPU)
+                if (c.Hardware[Convert.ToInt32(i)].HardwareType == HardwareType.CPU)
                 {
-                    //
-                    hardware.Update();
-                    foreach (var sensor in hardware.Sensors)
+                    for (float j = 0; j < c.Hardware[Convert.ToInt32(i)].Sensors.Length; j++)
                     {
-                        // store
-                        cpuTemp = sensor.Value.GetValueOrDefault();
-                        System.Diagnostics.Debug.WriteLine(cpuTemp);
-                        command.Parameters.AddWithValue("@temperature", cpuTemp);
-                        var result = command.ExecuteNonQuery();
-                        
-                        Console.WriteLine("Rows added : {0}", result);
+                        if (c.Hardware[Convert.ToInt32(i)].Sensors[Convert.ToInt32(j)].SensorType == SensorType.Temperature)
+                        {
+                            cpuTemp = c.Hardware[Convert.ToInt32(i)].Sensors[Convert.ToInt32(j)].Value.GetValueOrDefault();
+                            System.Diagnostics.Debug.WriteLine(cpuTemp);
+                            command.Parameters.AddWithValue("@temperature", cpuTemp);
+                            var result = command.ExecuteNonQuery();
+                            Console.WriteLine("Rows added : {0}", result);
+                        }
                     }
                 }
                 else
@@ -49,6 +66,9 @@ namespace CPUTemp2
                     databaseObject.CloseConnection();
                 }
             }
+
+            //
+           
         }
 
         static void Main(string[] args)
