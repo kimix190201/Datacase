1354ee1db5274e304685288826076905557cb922
Matias <matias@matias.wtf>
2021-12-18 18:20:37 +0200

    juuh

--- CPUTemp2/CPUTemp2/Program.cs
+++ CPUTemp2/CPUTemp2/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenHardwareMonitor.Hardware;
-using System.Data.SQLite;
 using MySql.Data.MySqlClient;
 
 namespace CPUTemp2
